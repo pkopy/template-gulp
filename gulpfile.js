b3d5e84987965a31172d9bb11af3ff446a3c7f38
pkopy <pawel.kopycki@gmail.com>
2018-07-06 08:03:43 +0200

    Init

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('default', () => console.log("test"))
+
+gulp.task('styles', function()  {
+    gulp.src('./sass/*.scss')
+        .pipe(sass({
+            outputStyle: 'compact'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist/css'))
+})
